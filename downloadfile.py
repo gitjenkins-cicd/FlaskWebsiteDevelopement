11630f7b5b8b38e0b8d29bf11fba063df34b6022
gitjenkins-cicd <gitjenkins2020@gmail.com>
2023-10-03 17:46:57 +0530

    logic to download

--- downloadfile.py
+++ downloadfile.py
@@ -1,7 +1,12 @@
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, render_template
 from app import app
 @app.route('/download', methods=['GET'])
 def download_file():
     filename = request.args.get('filename')
-    file_path = f'D:/HotTechnologies/python/FlaskWebsiteDevelopement/files/{filename}'  # Replace with the actual file path
-    return send_file(file_path, as_attachment=True)
+    try:
+        file_path = f'D:/HotTechnologies/python/FlaskWebsiteDevelopement/files/{filename}'  # Replace with the actual file path
+        return send_file(file_path, as_attachment=True) 
+        # return render_template("downloadfile.html")
+        # return render_template("downloadfile.html", name=filename)
+    except FileNotFoundError:
+        return "File not found."
